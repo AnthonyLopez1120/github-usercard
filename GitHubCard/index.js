f3daa248f6142c4c54c7a2a4e406d4ed67fd4dd7
Anthony Lopez <anthonybeconscious@gmail.com>
2019-10-31 17:11:29 -0600

    finished followers cards

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,40 +1,39 @@
-/* Step 1: using axios, send a GET request to the following URL 
-           (replacing the palceholder with your Github name):
-           https://api.github.com/users/<your name>
-*/
+
+let headerDiv = document.querySelector('.cards')
 axios.get('https://api.github.com/users/AnthonyLopez1120')
   .then(response =>{
     console.log(response.data);
     let stuff = response.data;
-    let headerDiv = document.querySelector('.cards')
     headerDiv.appendChild(gitHubCard(stuff))
+
+
   })
   .catch(error => {
     console.log("Error:", err);
 })
 
-/* Step 2: Inspect and study the data coming back, this is YOUR 
-   github info! You will need to understand the structure of this 
-   data in order to use it to build your component function 
 
-   Skip to Step 3.
-*/
 
-/* Step 4: Pass the data received from Github into your function, 
-           create a new component and add it to the DOM as a child of .cards
-*/
 
-/* Step 5: Now that you have your own card getting added to the DOM, either 
-          follow this link in your browser https://api.github.com/users/<Your github name>/followers 
-          , manually find some other users' github handles, or use the list found 
-          at the bottom of the page. Get at least 5 different Github usernames and add them as
-          Individual strings to the friendsArray below.
-          
-          Using that array, iterate over it, requesting data for each user, creating a new card for each
-          user, and adding that card to the DOM.
-*/
+const followersArray = ["emilyelri", "lyndsiWilliams", "cmruss", "bseverino", "wsu718"];
 
-const followersArray = [];
+followersArray.forEach(item=>{
+  axios.get(`https://api.github.com/users/${item}`)
+  .then(
+    response=>{
+        console.log(response.data)
+        let followersInfo = response.data
+        headerDiv.appendChild(gitHubCard(followersInfo))
+  
+      
+    })
+        
+})
+
+
+  
+
+
 function gitHubCard(obj){
   let card = document.createElement('div')
   let pic = document.createElement('img')
@@ -77,30 +76,5 @@ function gitHubCard(obj){
 
   return card;
 }
-/* Step 3: Create a function that accepts a single object as its only argument,
-          Using DOM methods and properties, create a component that will return the following DOM element:
 
-<div class="card">
-  <img src={image url of user} />
-  <div class="card-info">
-    <h3 class="name">{users name}</h3>
-    <p class="username">{users user name}</p>
-    <p>Location: {users location}</p>
-    <p>Profile:  
-      <a href={address to users github page}>{address to users github page}</a>
-    </p>
-    <p>Followers: {users followers count}</p>
-    <p>Following: {users following count}</p>
-    <p>Bio: {users bio}</p>
-  </div>
-</div>
 
-*/
-
-/* List of LS Instructors Github username's: 
-  tetondan
-  dustinmyers
-  justsml
-  luishrd
-  bigknell
-*/
